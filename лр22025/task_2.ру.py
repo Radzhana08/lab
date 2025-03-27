6473dd2c66ee77de72224fad213ada2b5107abca
Radzhana08 <radzhana.tudupova@gmail.com>
2025-03-27 15:33:32 +0300

    Update task_2.ру.py

--- лр22025/task_2.ру.py
+++ лр22025/task_2.ру.py
@@ -23,24 +23,26 @@ class Book:
         return f'Книга "{self.name}"'
 
     def __repr__(self):
-        return f"Book(id_={self.id_}, name= 'self.name', pages={self.pages})"
+        return f"Book(id_={self.id_}, name='{self.name}', pages={self.pages})"
 
 # TODO написать класс Library
 class Library:
-    def __init__(self, books: int = 0):
+    def __init__(self, books=None):
+        if books is None:
+            books = []
         self.books = books
 
     def get_next_book_id(self):
         if not self.books:
             return 1
         else:
-            return self.books + 1
+            return self.books[-1].id_ + 1
 
-    def get_index_by_book_id(self,n):
-        if n <= self.books:
-            return n-1
-        else:
-            raise ValueError("Книги с запрашиваемым id не существуют")
+    def get_index_by_book_id(self, id_):
+        for index, book in enumerate(self.books):
+            if book.id_ == id_:
+                return index
+        raise ValueError("Книги с запрашиваемым id не существуют")
 
 if __name__ == '__main__':
     empty_library = Library()  # инициализируем пустую библиотеку
@@ -49,7 +51,7 @@ if __name__ == '__main__':
     list_books = [
         Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
     ]
-    library_with_books = Library(books=len(list_books))  # инициализируем библиотеку с книгами
+    library_with_books = Library(books=list_books)  # инициализируем библиотеку с книгами
     print(library_with_books.get_next_book_id())  # проверяем следующий id для непустой библиотеки
 
     print(library_with_books.get_index_by_book_id(1))  # проверяем индекс книги с id = 1
